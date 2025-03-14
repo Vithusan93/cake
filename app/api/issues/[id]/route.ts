410a473a30f953eda87745e47ed31328740900f9
Vithusan Gopalapillai <gvithusan@gmail.com>
2025-03-14 18:02:57 +0100

    fix catching build

---
+++ app/api/issues/[id]/route.ts
@@ -0,0 +1,30 @@
+import prisma from "@/prisma/client";
+import { NextResponse } from "next/server";
+import { issueSchema } from "../validationSchemas";
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  const body = await request.json();
+  const validation = issueSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400 });
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(id) },
+  });
+  if (!issue)
+    return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
+
+  const updatesIssue = await prisma.issue.update({
+    where: { id: issue.id },
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+  return NextResponse.json(updatesIssue, { status: 200 });
+}
+
+//https://github.com/vercel/next.js/issues/74127
