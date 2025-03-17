86b952c1bb0f8acaa0a6d8b1bc94e2373cb996d0
Vithusan Gopalapillai <gvithusan@gmail.com>
2025-03-17 23:10:58 +0100

    Fix avatar not loading

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
+const { Value } = require('@radix-ui/themes/components/data-list')
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async headers () {
+            return [
+                {
+                    source:"/:path*",
+                    headers:[
+                        {key: 'referrer-policy', value: 'no-referrer'}
+                    ]
+                }
+            ]
+    }
+}
 
 module.exports = nextConfig
